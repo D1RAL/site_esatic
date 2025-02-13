657e45e30004237135e00d61a1a7332a16e54b9d
ABE JEAN CHRIST EMMANUEL DAGOU <jeanchrist690@gmail.com>
2025-02-13 20:36:31 +0000

    ahbebih

--- site_esatic/site_esatic/wsgi.py
+++ site_esatic/site_esatic/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'site_esatic.settings')
 
 application = get_wsgi_application()
+
+"""bonjour les amis"""
