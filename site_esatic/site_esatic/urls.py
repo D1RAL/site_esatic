efe714b8c4bd9a44b25a023319fed74956582b6c
D1RAL <isaaczokou2006@gmail.com>
2025-02-17 14:23:20 +0000

    Reapply "Configuration generale du projet"
    This reverts commit e00db3a9d4d4a0ae3c9afe23389562c06f0a3aec.

--- site_esatic/site_esatic/urls.py
+++ site_esatic/site_esatic/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('etudiant/', include('etudiant.urls')),
+    path('professeurs/', include('professeurs.urls')),
+    path('administration/', include('administration.urls'))
 ]
